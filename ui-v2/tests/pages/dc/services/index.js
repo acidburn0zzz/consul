bccf850c980ede086ce8467b2518cd5dc2b73647
kenia <keniavalladarez@gmail.com>
2020-04-07 12:46:41 -0400

    Create and update tests for service-list page

--- ui-v2/tests/pages/dc/services/index.js
+++ ui-v2/tests/pages/dc/services/index.js
@@ -1,11 +1,12 @@
 export default function(visitable, clickable, attribute, collection, page, filter) {
+  const service = {
+    name: attribute('data-test-service'),
+    service: clickable('a'),
+    externalSource: attribute('data-test-external-source', '[data-test-external-source]'),
+  };
   return {
     visit: visitable('/:dc/services'),
-    services: collection('[data-test-service]', {
-      name: attribute('data-test-service'),
-      service: clickable('a'),
-      externalSource: attribute('data-test-external-source', 'a span'),
-    }),
+    services: collection('[data-test-service]', service),
     dcs: collection('[data-test-datacenter-picker]', {
       name: clickable('a'),
     }),
